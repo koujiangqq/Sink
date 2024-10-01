8025c88967e3c7f809e4d1f8fd23be2bf1f90440
koujiangqq <125171010+koujiangqq@users.noreply.github.com>
2024-10-01 14:40:02 +0800

    Update Breadcrumb.vue

--- components/dashboard/Breadcrumb.vue
+++ components/dashboard/Breadcrumb.vue
@@ -14,7 +14,7 @@ defineProps({
     <BreadcrumbList>
       <BreadcrumbItem>
         <BreadcrumbLink href="/">
-          Sink
+          短短好用
         </BreadcrumbLink>
       </BreadcrumbItem>
       <BreadcrumbSeparator />
@@ -23,7 +23,7 @@ defineProps({
           :as="NuxtLink"
           to="/dashboard"
         >
-          Dashboard
+          仪表板
         </BreadcrumbLink>
       </BreadcrumbItem>
       <BreadcrumbSeparator />
