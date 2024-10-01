03fc9c2d0a3006d2e0117fd0627921f116aede70
koujiangqq <125171010+koujiangqq@users.noreply.github.com>
2024-10-01 12:01:26 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
-  title: 'Sink',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  title: '短短好用',
+  description: '一个简单快速，具有分析功能的短链接生成工具',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
