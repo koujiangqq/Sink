bdd144e74324cfc6dc3a68ce48b90b7306503692
koujiangqq <125171010+koujiangqq@users.noreply.github.com>
2024-10-01 18:22:53 +0800

    Update Header.vue

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -61,7 +61,7 @@ const showMenu = ref(false)
             >
               <a
                 class="w-full px-6 py-2 mr-0 text-gray-700 cursor-pointer dark:text-gray-300 md:px-3 md:mr-2 lg:mr-3 md:w-auto"
-                <GitHubIcon
+                <GoogleChromeIcon
                   class="w-5 h-5 mr-1"
                 />
                 href="https://web.mozui.cn"
@@ -74,7 +74,7 @@ const showMenu = ref(false)
                 title="开发者博客"
                  class="w-full px-6 py-2 mr-0 text-gray-700 cursor-pointer dark:text-gray-300 md:px-3 md:mr-2 lg:mr-3 md:w-auto"
               >
-                <GitHubIcon
+                <FacebookIcon
                   class="w-5 h-5 mr-1"
                 />
                 博客</a>
@@ -84,7 +84,7 @@ const showMenu = ref(false)
                 title="网盘资源分享"
                  class="w-full px-6 py-2 mr-0 text-gray-700 cursor-pointer dark:text-gray-300 md:px-3 md:mr-2 lg:mr-3 md:w-auto"
               >
-                <GitHubIcon
+                <MacOsIcon
                   class="w-5 h-5 mr-1"
                 />
                 资源</a>
