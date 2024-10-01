ac378936a0bdfde2bdfacd5746bb3adedfa22c6d
koujiangqq <125171010+koujiangqq@users.noreply.github.com>
2024-10-01 15:13:30 +0800

    Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -13,22 +13,22 @@ onMounted(() => {
       <a
         href="/"
         class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
-        title="Sink"
+        title="短短好用"
       >Sink</a>
       <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://html.zone"
+        href="https://mozui.cn"
         target="_blank"
-        title="HTML.ZONE"
+        title="WWW.MOZUI.CN"
       >
-        &copy; {{ new Date().getFullYear() }} Products of HTML.ZONE
+        &copy; {{ new Date().getFullYear() }} Power by Mozui
       </a>
       <span
         class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
       >
         <a
           ref="email"
-          href="mailto:sink.cool$miantiao.me"
+          href="mailto:missyouling$gmail.com"
           title="Email"
           class="text-gray-400 hover:text-gray-500"
         >
@@ -38,7 +38,7 @@ onMounted(() => {
           />
         </a>
         <a
-          href="https://t.me/htmlzone"
+          href="https://t.me/MozuiCN"
           target="_blank"
           title="Telegram"
           class="text-gray-400 hover:text-gray-500"
@@ -49,7 +49,7 @@ onMounted(() => {
           />
         </a>
         <a
-          href="https://mt.ci"
+          href="https://blog.mozui.cn"
           target="_blank"
           title="Blog"
           class="text-gray-400 hover:text-gray-500"
@@ -59,33 +59,9 @@ onMounted(() => {
             class="w-6 h-6"
           />
         </a>
-
+        
         <a
-          href="https://x.com/0xKaiBi"
-          target="_blank"
-          title="Twitter"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Twitter</span>
-          <XIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          href="https://miantiao.me/@chi"
-          target="_blank"
-          title="Mastodon"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Mastodon</span>
-          <MastodonIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          href="https://github.com/ccbikai"
+          href="https://github.com/koujiangqq"
           target="_blank"
           title="GitHub"
           class="text-gray-400 hover:text-gray-500"
